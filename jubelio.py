8644de988f0ddf568d434b865ed6c66061f6e7bc
faizallramadhan <faizal.ramadhan007@gmail.com>
2023-06-30 16:14:37 +0700

    commit 2

--- jubelio.py
+++ jubelio.py
@@ -1,10 +1,10 @@
-import pytest
 from selenium import webdriver
-from pytest_html import attach
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import pytest
 
+@pytest.fixture(scope="module")
 def driver():
     driver = webdriver.Chrome()
     yield driver
@@ -12,5 +12,22 @@ def driver():
 
 def login_test(driver):
     driver.get("https://https://app.jubelio.com/login")
+    
+    username_input = driver.find_element(By.ID, "username")
+    password_input = driver.find_element(By.ID, "password")
+    username_input.send_keys("your_username")
+    password_input.send_keys("your_password")
+
+    # Submit the login form
+    login_button = driver.find_element(By.ID, "login-button")
+    login_button.click()
+
+    # Wait for the dashboard page to load
+    dashboard_title = WebDriverWait(driver, 10).until(
+        EC.title_contains("Dashboard")
+    )
+
+    # Verify the successful login by checking the title of the dashboard page
+    assert "Dashboard" in driver.title, "Login failed!"
 
 
